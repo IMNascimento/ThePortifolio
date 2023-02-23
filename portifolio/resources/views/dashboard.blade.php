ac715bb18b1e80ecf9b4cca0f88ea6d9623f697f
Igor Nascimento <trabalho.computador.sjn@gmail.com>
2023-02-23 20:33:33 -0300

    criei a página dashboard

--- portifolio/resources/views/dashboard.blade.php
+++ portifolio/resources/views/dashboard.blade.php
@@ -1,17 +1,74 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
-
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
-                    {{ __("You're logged in!") }}
-                </div>
-            </div>
-        </div>
+@extends('layouts.layout')
+@section('title', 'Dashboard')
+@section('content')
+ 
+<!-- navbar dashboard bootstrap 4.6-->
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+      <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+        <li class="nav-item active">
+          <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-expanded="false">
+              About
+            </a>
+            <ul class="dropdown-menu">
+              <li><a class="dropdown-item" href="#">ADD</a></li>
+              <li><a class="dropdown-item" href="#">LIST</a></li>
+            </ul>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-expanded="false">
+              Service
+            </a>
+            <ul class="dropdown-menu">
+              <li><a class="dropdown-item" href="#">ADD</a></li>
+              <li><a class="dropdown-item" href="#">LIST</a></li>
+            </ul>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-expanded="false">
+              Portfolio
+            </a>
+            <ul class="dropdown-menu">
+              <li><a class="dropdown-item" href="#">ADD</a></li>
+              <li><a class="dropdown-item" href="#">LIST</a></li>
+            </ul>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-expanded="false">
+              Testimonials
+            </a>
+            <ul class="dropdown-menu">
+              <li><a class="dropdown-item" href="#">ADD</a></li>
+              <li><a class="dropdown-item" href="#">LIST</a></li>
+            </ul>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-expanded="false">
+              Signature
+            </a>
+            <ul class="dropdown-menu">
+              <li><a class="dropdown-item" href="#">ADD</a></li>
+              <li><a class="dropdown-item" href="#">LIST</a></li>
+            </ul>
+          </li>
+      </ul>
     </div>
-</x-app-layout>
+  </nav>
+<!--fim navbar dashboard-->
+    
+
+
+
+
+
+
+
+
+@endsection
